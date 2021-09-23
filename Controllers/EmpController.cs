870d35e9567b19e0c342f4cdca2fb4ab78c0a39f
akashca <91238247+akashca@users.noreply.github.com>
2021-09-23 12:24:02 +0530

    Add files via upload

---
+++ Controllers/EmpController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MvcCore;
+using Microsoft.EntityFrameworkCore;
+using MvcCore.Models;
+namespace MvcCore.Controllers
+{
+    public class EmpController : Controller
+    {
+        private readonly ApplicationDbContext _db; 
+        public EmpController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var displaydata = _db.Employeetable.ToList();
+            return View(displaydata);
+        }
+
+        public IActionResult create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>create(NewEmpClass nec)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Add(nec);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(nec);
+        }
+
+        public async Task<IActionResult> Edit(int? id   )
+        {
+            if (id==null)
+            {
+                return RedirectToAction("Index");
+            }
+            var getemployeedetails = await _db.Employeetable.FindAsync(id);
+            return View(getemployeedetails);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(NewEmpClass nc)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(nc);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(nc);
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var getemployeedetails = await _db.Employeetable.FindAsync(id);
+            return View(getemployeedetails);
+        }
+
+       //[HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var getemployeedetails = await _db.Employeetable.FindAsync(id);
+            return View(getemployeedetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var getemployeedetails = await _db.Employeetable.FindAsync(id);
+            _db.Employeetable.Remove(getemployeedetails);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/NewEmpClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace MvcCore.Models
+{
+    public class NewEmpClass
+    {
+        [Key]
+        public int Empid { get; set; }
+
+        [Required(ErrorMessage ="Enter employee Name")]
+        [Display(Name ="Employee Name")]
+        public string EmpName { get; set; }
+
+        [Required(ErrorMessage = "Enter employee Email")]
+        [Display(Name = " Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter employee Age")]
+        [Display(Name = " Age")]
+        [Range(20,60)]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Enter employee Salary")]
+        [Display(Name = " Salary")]
+        public int Salary { get; set; }
+
+    }
+}
